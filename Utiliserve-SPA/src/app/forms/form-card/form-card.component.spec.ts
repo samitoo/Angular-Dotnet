8e6e14539e09305ae82ab29bcaafa21a8ec3c342
Samitoo <sammy.hamdan@gmail.com>
2019-08-25 20:19:48 -0700

    Added landing page, dashboard

---
+++ Utiliserve-SPA/src/app/forms/form-card/form-card.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { FormCardComponent } from './form-card.component';
+
+describe('FormCardComponent', () => {
+  let component: FormCardComponent;
+  let fixture: ComponentFixture<FormCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FormCardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
